4ac28d296ccaf9d8b0b1cf9fdb19b4934e5a09b9
taynacosta <taynamoreira1811@gmail.com>
2021-06-26 16:47:47 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/taynacosta/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.taynacosta.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.taynacosta.workshopmongo.domain.User;
+import com.taynacosta.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll(); // apaga o q tinhamos feito manualmente no banco
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
