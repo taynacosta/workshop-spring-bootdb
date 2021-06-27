74f4dd7011cb7fd1e0e97fc2d9780430da5eed31
taynacosta <taynamoreira1811@gmail.com>
2021-06-27 14:21:38 -0300

    Obtendo um post por id

---
+++ src/main/java/com/taynacosta/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.taynacosta.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taynacosta.workshopmongo.domain.Post;
+import com.taynacosta.workshopmongo.repository.PostRepository;
+import com.taynacosta.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
