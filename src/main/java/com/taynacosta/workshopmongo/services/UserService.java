a9208af7edff62cb9482cc0bba7453968a46490a
taynacosta <taynamoreira1811@gmail.com>
2021-06-27 07:42:34 -0300

    Obtendo um usuário por id

--- src/main/java/com/taynacosta/workshopmongo/services/UserService.java
+++ src/main/java/com/taynacosta/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.taynacosta.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.taynacosta.workshopmongo.domain.User;
 import com.taynacosta.workshopmongo.repository.UserRepository;
+import com.taynacosta.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,9 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
