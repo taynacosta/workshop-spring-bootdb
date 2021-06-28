35981febcce93b74b4e95d19d74006daf72f8a7c
taynacosta <taynamoreira1811@gmail.com>
2021-06-28 19:46:07 -0300

    Consulta com vários critérios

--- src/main/java/com/taynacosta/workshopmongo/services/PostService.java
+++ src/main/java/com/taynacosta/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.taynacosta.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,4 +25,8 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 	
+	public List<Post> fullSeach(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
 }
